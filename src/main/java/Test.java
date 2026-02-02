b7a8b23d8a04d2f3952ea343e8a19757b94628ca
JavierReyes1 <c00312829@setu.ie>
2026-02-02 13:31:25 +0000

    Testing

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,27 +4,32 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Test {
-    public static void main(String[] args) {
-        final String DATABASE_URL = "jdbc:mariadb://localhost:3306/test_table";
-        final String USER = "root";
-        final String PASSWORD = "pas123";
-        
-        String firstName = "Javier";
-        String lastName = "Reyes";
-        
-        try (Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-             PreparedStatement pstat = connection.prepareStatement(
-                 "INSERT INTO Authors (FirstName, LastName) VALUES(?,?)")) {
-            
-            pstat.setString(1, firstName);
-            pstat.setString(2, lastName);
-            
-            int rowsAffected = pstat.executeUpdate();
-            System.out.println(rowsAffected + " record successfully added to the table.");
-            
-        } catch (SQLException e) {
-            System.err.println("Database error occurred:");
-            e.printStackTrace();
-        }
-    }
-}
+	final static String DATABASE_URL = "jdbc:mariadb://localhost:3306/test_table";
+	final static String USER = "root";
+	final static String PASSWORD = "pas123";
+
+	static String firstName = "Javier";
+	static String lastName = "Reyes";
+
+	public static void insert() {
+		try (Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+				PreparedStatement pstat = connection.prepareStatement(
+						"INSERT INTO Authors (FirstName, LastName) VALUES(?,?)")) {
+
+			pstat.setString(1, firstName);
+			pstat.setString(2, lastName);
+
+			int rowsAffected = pstat.executeUpdate();
+			System.out.println(rowsAffected + " record successfully added to the table.");
+
+		} catch (SQLException e) {
+			System.err.println("Database error occurred:");
+			e.printStackTrace();
+		}
+	}
+
+	public static void 
+
+	public static void main(String[] args) {
+	}// end main
+}// end class
